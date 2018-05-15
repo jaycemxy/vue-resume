49af52b1ecbcbf1676239e3128401b257cfde9fc
JayceMXY <jayce_ma.xa@foxmail.com>
2018-05-15 16:05:39 +0800

    登录、注册功能

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,33 @@
-var app = new Vue({
+let app = new Vue({
     el: '#app',
     data: {
         editingName: false,
+        loginVisible: false,
+        signUpVisible: false,
         resume: {
             name: '姓名',
             gender: '女',
-            birthday: '1996.02',
+            birthday: '1990年1月',
             jobTitle: '前端工程师',
-            phone: '18292034074',
-            email: 'xxxxxxxxx@qq.com'
+            phone: '138111111111',
+            email: 'example@example.com'
         }
     },
-    method: {
+    methods: {
         onEdit(key, value) {
             this.resume[key] = value
         },
+        onClickSave() {
+            let currentUser = AV.User.current()
+            if (!currentUser) {
+                this.loginVisible = true
+            } else {
+                this.saveResume()
+            }
+        },
+        saveResume() {
+
+        },
+
     }
-})
+});
