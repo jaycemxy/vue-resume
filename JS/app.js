229b7d8935c1a73c15b7c4c1f16379ea20774485
JayceMXY <jayce_ma.xa@foxmail.com>
2018-05-15 18:12:25 +0800

    保存resume信息

--- JS/app.js
+++ JS/app.js
@@ -4,6 +4,11 @@ let app = new Vue({
         editingName: false,
         loginVisible: false,
         signUpVisible: false,
+        currentUser: {
+            id: undefined,
+            email: '',
+            fuck: 'fuck'
+        },
         resume: {
             name: '姓名',
             gender: '女',
@@ -11,11 +16,43 @@ let app = new Vue({
             jobTitle: '前端工程师',
             phone: '138111111111',
             email: 'example@example.com'
-        }
+        },
+        login: {
+            email: '',
+            password: ''
+        },
+        signUp: {
+            email: '',
+            password: ''
+        },
     },
     methods: {
         onEdit(key, value) {
             this.resume[key] = value
+        },
+        onLogin(e) {
+            AV.User.logIn(this.login.email, this.login.password).then((user) => {
+                this.currentUser.id = user.id
+                this.currentUser.email = user.attributes.email
+            }, (error) => {
+                if (error.code === 211) {
+                    alert('邮箱不存在')
+                } else if (error.code === 210) {
+                    alert('邮箱和密码不匹配')
+                }
+            })
+        },
+        onLogout(e) {
+            AV.User.logOut();
+            alert('注销成功')
+            window.location.reload()
+        },
+        onSignUp(e) {
+            const user = new AV.User()
+            user.setUsername(this.signUp.email)
+            user.setPassword(this.signUp.password)
+            user.setEmail(this.signUp.email)
+            user.signUp().then((user) => {}, (error) => {})
         },
         onClickSave() {
             let currentUser = AV.User.current()
@@ -26,8 +63,15 @@ let app = new Vue({
             }
         },
         saveResume() {
-
+            let {id} = AV.User.current()
+            let user = AV.Object.createWithoutData('User', id)
+            user.set('resume', this.resume)
+            user.save()
         },
-
     }
-});
+})
+
+let currentUser = AV.User.current()
+if (currentUser) {
+    app.currentUser = currentUser.toJSON()
+}
